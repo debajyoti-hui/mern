46a8b264501755f40f694015509b9a3109fa5ed1
deba17072002 <deba17072002@gmail.com>
2024-10-04 11:05:07 +0000

    js basics added

--- C1Week1/jsBasic.js
+++ C1Week1/jsBasic.js
@@ -16,29 +16,36 @@ console.log(users[users.length - 2]);
 console.log(users[users.length - 3]);
 console.log(users[users.length - 4]);
 
+// Loop through the users array using a traditional for loop
 for (let i = 0; i < users.length; i++) {
     console.log(users[i]);
 }
 
+// Loop through the users array using a for...of loop
 for (let user of users) {
     console.log(user);
 }
 
+// Loop through the users array using a for...in loop
 for (let i in users) {
     console.log(users[i]);
 }
 
+// Loop through the users array using a while loop
 let i = 0;
 while (i < users.length) {
   console.log(users[i]);
   i++;
 }
 
+// Remove and log each user from the array until it's empty
 while (users.length > 0) {
     console.log(users.pop());
 }
 
+// Log the empty users array
 console.log(users);
+
 
 
 
